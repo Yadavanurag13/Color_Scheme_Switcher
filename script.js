eed129ee8c87085cf6580501f36aecdde2f766db
Anurag Yadav <yadavanurag1310@gmail.com>
2024-03-09 17:50:18 +0530

    final submit

--- script.js
+++ script.js
@@ -1,6 +1,12 @@
+//we selected out all the class that named as button
 const buttons = document.querySelectorAll('.button')
+//this will return out nodelist
+
+//we selected out the body of the webpage so that we
+//can make changess
 const body = document.querySelector('body')
 
+//used for eachloop to get access of the buttons we can use only for each loop on nodelist
 buttons.forEach(function(button) {
     //console.log(button);
 
@@ -8,7 +14,7 @@ buttons.forEach(function(button) {
         console.log(event);
         console.log(event.target);
 
-
+        //checking each condition and changing the color according to that
         // if(event.target.id === 'grey') {
         //     body.style.backgroundColor = event.target.id;
         // }
